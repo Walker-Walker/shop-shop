4757aac806d5995bbbddaef4d68920ef42dec23f
Walker-Walker <awalker0@gmail.com>
2020-10-14 19:51:06 -0700

    added tests for all reducers, completed shopping cart ui feature

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,21 +1,52 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useQuery } from '@apollo/react-hooks';
+import Cart from '../components/Cart';
 
 import { QUERY_PRODUCTS } from "../utils/queries";
 import spinner from '../assets/spinner.gif'
 import{ useStoreContext } from "../utils/GlobalState";
-import{ UPDATE_PRODUCTS } from "../utils/actions";
+import{ ADD_TO_CART, UPDATE_PRODUCTS, REMOVE_FROM_CART,UPDATE_CART_QUANTITY } from "../utils/actions";
+//actions
+
+
 
 function Detail() {
-  const [state, dispatch] = useStoreContext();
+
+const addToCart= () => {
+const itemInCart = cart.find((cartItem) => cartItem._id === id);
+
+if (itemInCart) {
+  dispatch({
+    type: UPDATE_CART_QUANTITY,
+    _id: id,
+    purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+  });
+
+} else {
+  dispatch({
+    type: ADD_TO_CART,
+    product: { ...currentProduct, purchaseQuantity: 1 }
+  });
+}
+};
+
+const removeFromCart = () => {
+  dispatch( {
+    type: REMOVE_FROM_CART,
+    _id: currentProduct._id
+  });
+};
+const [state, dispatch] = useStoreContext();
 const { id } = useParams();
 
 const [currentProduct, setCurrentProduct] = useState({})
 
 const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-const { products } = state;
+const { products, cart } = state;
+
+
 
 useEffect(() => {
   if (products.length) {
@@ -62,10 +93,11 @@ useEffect(() => {
             <strong>Price:</strong>
             ${currentProduct.price}
             {" "}
-            <button>
+            <button onClick={addToCart}>
               Add to Cart
             </button>
-            <button>
+            <button disabled={!cart.find(p => p._id === currentProduct._id)}
+            onClick={removeFromCart}>
               Remove from Cart
             </button>
           </p>
@@ -79,6 +111,7 @@ useEffect(() => {
       {
         loading ? <img src={spinner} alt="loading" /> : null
       }
+      <Cart />
     </>
   );
 };
